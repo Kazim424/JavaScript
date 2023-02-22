67bbd745e6a5a1f90ae8aa4d22279023a15fe77a
Mohammad Kazim <120630543+Kazim424@users.noreply.github.com>
2023-02-22 04:14:27 -0500

    Loops problems
    Loops

---
+++ Loops/loop1.js
@@ -0,0 +1,8 @@
+// Using loop to solve the problem.
+
+// Question1:-1. Write a program that generates the multiplication table in the textbook format for a given number.
+
+let tableof = 12;
+ for(let i = 1; i<=10; i++){
+    console.log(`${tableof} * ${i} = ${tableof * i}`);
+ }
---
+++ Loops/loop2.js
@@ -0,0 +1,25 @@
+// Using loop to solve the problem
+
+// Question:2- Write a program that prints all the positive even numbers till the number specified.
+
+
+// using for loop
+
+// let num = 20;
+// for(let i =1; i <= num; i++ ){
+// if(i %2 ==0){
+// console.log(i);
+// }
+// }
+
+
+// using while loop
+
+let num1 = 15;
+let i =1;
+while( i <= num1  ){
+if(i %2 ==0){
+    console.log(i);
+}
+i++;
+}
