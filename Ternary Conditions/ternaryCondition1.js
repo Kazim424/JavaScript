c54bf3ac1445a47767e344f47fa7639b705a3e8f
Mohammad Kazim <120630543+Kazim424@users.noreply.github.com>
2023-02-22 04:12:59 -0500

    Add files via upload

---
+++ Ternary Conditions/ternaryCondition1.js
@@ -0,0 +1,10 @@
+// Using Ternary Conditions
+
+// Question:1- Write a program that takes in a number and outputs whether it is positive, negative, or zero.
+
+let number = 6;
+ number == 0
+ ? console.log(`Number is Zero`)
+ :number > 0
+ ? console.log(`Number is Positive `)
+: console.log(`Number is Negative `);
---
+++ Ternary Conditions/ternaryCondition2.js
@@ -0,0 +1,12 @@
+// Using Ternary Conditions
+
+// Question-2. Create a program that takes in two numbers and prints the larger one.
+
+let num1 = 8;
+let num2 = 5;
+
+num1 == num2
+? console.log(`Both number are equal`)
+: num1 > num2
+? console.log(`The larger number among two number is ${num1}`)
+:console.log(`The laregr number among two number is ${num2}`);
